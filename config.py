cb4da6c0aefbc8f4cfc01ca842f925b75c5d677b
meet1027 <meetck27@gmail.com>
2025-04-13 15:14:14 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+INDICATORS = [
+    "macd", "boll_ub", "boll_lb", "rsi_30", "cci_30", 
+    "dx_30", "close_30_sma", "close_60_sma"
+]
+
+TICKERS = [
+    "RELIANCE.NS", "INFY.NS", "TCS.NS", "HDFCBANK.NS",
+    "ICICIBANK.NS", "SBIN.NS", "ADANIENT.NS", "PAYTM.NS",
+    "ZOMATO.NS", "TATAMOTORS.NS"
+]
+
+START_DATE = "2007-01-01"
+END_DATE = "2025-01-01"
